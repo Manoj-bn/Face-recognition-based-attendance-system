81ca9db6617d996f523fb06e1e4398db30cf96e8
manoj <manojnarayan0704@gmail.com>
2020-11-27 16:46:39 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+import face_recognition
+
+imgElon = face_recognition.load_image_file('ImagesMain/musk.jpg')
+imgElon = cv2.cvtColor(imgElon, cv2.COLOR_BGR2RGB)
+imgTest = face_recognition.load_image_file('ImagesMain/gates.jpg')
+imgTest = cv2.cvtColor(imgTest, cv2.COLOR_BGR2RGB)
+
+faceLoc = face_recognition.face_locations(imgElon)[0]
+encodeElon = face_recognition.face_encodings(imgElon)[0]
+cv2.rectangle(imgElon, (faceLoc[3], faceLoc[0], faceLoc[1], faceLoc[2]), (255, 0, 255), 2)
+
+faceLoctest = face_recognition.face_locations(imgTest)[0]
+encodetest = face_recognition.face_encodings(imgTest)[0]
+cv2.rectangle(imgTest, (faceLoctest[3], faceLoctest[0], faceLoctest[1], faceLoctest[2]), (255, 0, 255), 2)
+
+results = face_recognition.compare_faces([encodeElon],encodetest)
+faceDis = face_recognition.face_distance([encodeElon],encodetest)
+print(results,faceDis)
+cv2.putText(imgTest,f'{results} {round(faceDis[0],2)}',(50,50),cv2.FONT_ITALIC,1,(0,0,255),2)
+
+cv2.imshow('musk', imgElon)
+cv2.imshow('test', imgTest)
+cv2.waitKey(0)
